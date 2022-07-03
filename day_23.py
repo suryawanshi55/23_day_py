a1ab8c673841ecd26092d48ff4fe75708ecc96a0
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-03 18:34:45 +0530

    Add files via upload

---
+++ day_23.py
@@ -0,0 +1,45 @@
+# for w in range(1,5) :      #1
+    # print(w)               #2
+                             #3
+                             #4  
+# for r in range(4,5) :
+#    print(r)                 #4
+# for t in range(3,6) :      #3
+    # print(t)               #4
+                             #5
+# for e in range(1,4) :     #!                       
+    # print(e)              #2
+                            #3  
+# for n in range(0,10,2) : #0                          
+    # print(n)             #2
+                           #4
+                           #6
+                           #8
+# for n in range(0,10,7) : #0
+    # print(n)             #7
+# for n in range(1,10,2) :#1
+    # print(n)            #3
+                          #5
+                          #7
+                          #9
+# for r in range(2,10,2) :#2
+    # print(r)            #4
+                          #6
+                          #8
+# for n in range(10,1,-1) :   #10
+#    print(n)                 #9
+                              #8
+                              #7
+                              #6
+                              #5
+                              #4
+                              #3
+                              #2
+# for n in range(10,0,-2) :   #10
+    # print(n)                #8
+                              #6
+                              #4
+# for j in range(4,10,4) :    #4
+#     print(j)                #8
+for u in range(5,15,5) :
+    print(u)
